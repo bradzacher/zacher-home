a2a06916c58e6b0c73799e0fd391fb4f789b1ac6
Brad Zacher <brad.zacher@gmail.com>
2015-04-07 16:22:32 +0930

    IE11 compatability fixes
    If only it supported the shadow dom...

--- src/js/script.js
+++ src/js/script.js
@@ -4,31 +4,33 @@
     var projects = [
         {
             name: 'DotA2 Stats',
-            link: 'http://dota.zacher.com.au/',
+            demoLink: 'http://dota.zacher.com.au/',
             description: 'A match stats site built using angularjs.',
             img: 'dota2-stats.png'
         },
         {
             name: 'Eaglesong',
-            link: 'https://github.com/bradzacher/Eaglesong',
+            sourceLink: 'https://github.com/bradzacher/Eaglesong',
             description: 'A DotA2 replay parser, written in .NET.',
             img: 'Eaglesong.png'
         },
         {
             name: 'From Pictures to Programs with Genetic Programming',
-            link: 'https://github.com/bradzacher/HonsRecursiveSolverGE',
+            sourceLink: 'https://github.com/bradzacher/HonsRecursiveSolverGE',
             description: 'My honours thesis - evolves recursive solutions using GP.',
             img: 'HonsRecursiveSolverGE.png'
         },
         {
             name: 'cell-game',
-            link: 'http://zacher.com.au/git/cell-game/',
+            demoLink: 'http://zacher.com.au/git/cell-game/',
+            sourceLink: 'http://github.com/bradzacher/cell-game',
             description: 'The framework of a 2D top-down game (aka Pokemon) built using pure javascript and CSS.',
             img: 'cell-game.png'
         },
         {
             name: 'SpacePhysics',
-            link: 'http://zacher.com.au/git/SpacePhysics/',
+            demoLink: 'http://zacher.com.au/git/SpacePhysics/',
+            sourceLink: 'http://github.com/bradzacher/SpacePhysics',
             description: 'A playground for some physics simulations.',
             img: 'SpacePhysics.png'
         }
