9931e108491aa8779da5f26de088ac44ce7d324a
Brad Zacher <brad.zacher@gmail.com>
2018-07-01 21:54:40 +1000

    added codebuild scripts

---
+++ scripts/codebuild.sh
@@ -0,0 +1,11 @@
+# grab the parameters from SSM
+S3BUCKET=$(aws ssm get-parameters --names "/zacher-com-au/S3_BUCKET" --query Parameters[0].Value --output text)
+DISTRIBUTIONID=$(aws ssm get-parameters --names "/zacher-com-au/CF_DISTRIBUTION_ID" --query Parameters[0].Value --output text)
+
+# build
+yarn install
+yarn build
+
+# deploy
+aws s3 sync ./dist/ s3://$S3BUCKET/ --delete
+aws cloudfront create-invalidation --distribution-id=$DISTRIBUTIONID --paths=/*
