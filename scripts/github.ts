45d1c16677e4ebf5e0fd2198a73f45fae20dc054
Brad Zacher <brad.zacher@gmail.com>
2018-11-06 12:20:59 -0800

    [FIX] Fix github commits showing activity history as well

--- scripts/github.ts
+++ scripts/github.ts
@@ -28,6 +28,10 @@ export default async function github() {
 
     const res = dom.window.document.getElementById(divId)
 
+    // remove the overview from the DOM entirely.
+    // github-calendar just uses CSS to hide it, so there's no point in even shipping it
+    dom.window.document.getElementById('user-activity-overview').remove()
+
     fs.writeFileSync(path.resolve(buildPath, 'github-calendar.html'), res.outerHTML, 'utf8')
 }
 
