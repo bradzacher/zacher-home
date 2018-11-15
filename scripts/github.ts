e9ca839e807db06fc8c1a2bf39adda244fc960cf
Brad Zacher <brad.zacher@gmail.com>
2018-11-14 23:37:05 -0800

    Rewrite Github script so it works without a lib, and produces a react component

--- scripts/github.ts
+++ scripts/github.ts
@@ -1,38 +1,125 @@
 import * as fs from 'fs'
-import * as path from 'path'
-
 import 'isomorphic-fetch'
 import { JSDOM } from 'jsdom'
+import * as path from 'path'
+import { promisify } from 'util'
+import { parseString } from 'xml2js'
 
-import { createBuildFolder, buildPath } from './createBuildFolder'
+import { createGeneratedFolder } from './createBuildFolder'
 
-import GitHubCalendar = require('github-calendar')
+type Obj = Record<string, string>
+type ParsedSVG = {
+    svg : {
+        $ : Obj
+        g : Array<{
+            $ : Obj
+            g : Array<{
+                $ : Obj
+                rect : Array<{
+                    $ : Obj
+                }>
+            }>
+            text : Array<{
+                _ : string
+                $ : Obj
+            }>
+        }>
+    }
+}
+const parseStringAsync = promisify(parseString) as (xml : string) => Promise<ParsedSVG>
 
-const divId = 'github-calendar'
+function dumpAttributes(obj : { $: Record<string, string> }) {
+    const attrString = Object.keys(obj.$).map((k) => {
+        if (k === 'class') {
+            return `className={classes.${obj.$[k]}}`
+        }
 
-export default async function github() {
-    createBuildFolder()
-
-    // polyfill the browser
-    const dom = new JSDOM(`<div id="${divId}"></div>`)
-    global.window = dom.window
-    global.document = dom.window.document
-
-    await GitHubCalendar(`#${divId}`, 'bradzacher', {
-        global_stats: false,
-        proxy() {
-            return 'https://github.com/bradzacher'
-        },
-        responsive: true,
+        return `${k}='${obj.$[k]}'`
     })
 
-    const res = dom.window.document.getElementById(divId)
+    return attrString.join(' ')
+}
+function indent(lines : string[], size : number) {
+    const spaces = Array(size * 4)
+        .fill(' ')
+        .join('')
 
-    // remove the overview from the DOM entirely.
-    // github-calendar just uses CSS to hide it, so there's no point in even shipping it
-    dom.window.document.getElementById('user-activity-overview').remove()
+    return lines.map(l => `${spaces}${l}`)
+}
 
-    fs.writeFileSync(path.resolve(buildPath, 'github-calendar.html'), res.outerHTML, 'utf8')
+export default async function github() {
+    const outFolder = createGeneratedFolder()
+
+    // fetch the contribution graph
+    const response = await fetch('https://github.com/users/bradzacher/contributions', {
+        method: 'GET',
+    })
+    const rawHtml = await response.text()
+
+    // use JSDOM to get the svg element
+    // TODO - is it better to do this instead via parsing?
+    const dom = new JSDOM(rawHtml)
+    const res = dom.window.document.querySelector('svg.js-calendar-graph-svg')
+
+    const rawSvgStr = res.outerHTML
+        .split('\n')
+        // remove the hidden text elements
+        .filter(l => l.match(/style="display: none;"/) === null)
+        .join('\n')
+    const parsedSvg = await parseStringAsync(rawSvgStr)
+    delete parsedSvg.svg.$.class
+
+    const reactSvg = indent(
+        [
+            `<svg ${dumpAttributes(parsedSvg.svg)}>`,
+            `    <g ${dumpAttributes(parsedSvg.svg.g[0])}>`,
+            ...indent(
+                parsedSvg.svg.g[0].g.reduce((acc, g) => {
+                    acc.push(
+                        `<g ${dumpAttributes(g)}>`,
+                        ...indent(g.rect.map(rect => `<rect ${dumpAttributes(rect)} />`), 1),
+                        '</g>',
+                    )
+
+                    return acc
+                }, []),
+                3,
+            ),
+            ...indent(parsedSvg.svg.g[0].text.map(text => `<text ${dumpAttributes(text)}>${text._}</text>`), 3),
+            '    </g>',
+            '</svg>',
+        ],
+        1,
+    )
+
+    const lines = [
+        '/**',
+        ' * THIS FILE HAS BEEN AUTOMATICALLY GENERATED!',
+        ' *     ANY MANUAL CHANGES WILL BE LOST!',
+        ' */',
+        '/* eslint-disable max-len */// produces a nicer file than generating proper wrapped lines',
+        '',
+        'import * as React from \'react\'',
+        'import injectSheet, { WithSheet } from \'react-jss\'',
+        'import { createStyles } from \'../Theme\'',
+        '',
+        'const styles = createStyles(() => ({',
+        '    day: {},',
+        '    month: {},',
+        '    wday: {},',
+        '}))',
+        '',
+        'type Props = WithSheet<typeof styles>',
+        '',
+        'const GithubCalendar : React.FunctionComponent<Props> = ({ classes }) => (',
+        ...reactSvg,
+        ')',
+        '',
+        'export default injectSheet(styles)(GithubCalendar)',
+        '',
+    ]
+
+    fs.writeFileSync(path.resolve(outFolder, 'GithubCalendar.tsx'), lines.join('\n'), 'utf8')
 }
 
 github().catch((e) => {
