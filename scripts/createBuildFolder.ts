592b18275b62acfb6da8b69a218e6a8f7a56d8fa
Brad Zacher <brad.zacher@gmail.com>
2018-11-14 22:01:08 -0800

    reworked sprites build script

--- scripts/createBuildFolder.ts
+++ scripts/createBuildFolder.ts
@@ -1,7 +1,8 @@
 import * as fs from 'fs'
 import * as path from 'path'
 
-export const buildPath = path.resolve(__dirname, '../src/build')
+export const buildPath = path.resolve(__dirname, '../build')
+export const generatedPath = path.resolve(__dirname, '../src/app/generated')
 
 export function createBuildFolder() {
     try {
@@ -9,4 +10,15 @@ export function createBuildFolder() {
     } catch (_) {
         // ignored
     }
+
+    return buildPath
+}
+export function createGeneratedFolder() {
+    try {
+        fs.mkdirSync(generatedPath)
+    } catch (_) {
+        // ignored
+    }
+
+    return generatedPath
 }
