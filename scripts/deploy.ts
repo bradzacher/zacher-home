4c4c6d073dfdfe35f6e906eeffb4ea9bdb05f42f
Brad Zacher <brad.zacher@gmail.com>
2018-11-25 14:31:51 -0800

    Fix incorrect script lint

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -10,7 +10,7 @@ async function deploy() {
     // it's easier to call cli as the js-sdk doesn't include a sync method
     execSync(`aws s3 sync ./build/ s3://${s3bucket} --delete`)
     // again easier to call the cli as th js-sdk needs a lot of extra parameters
-    execSync(`ws cloudfront create-invalidation --distribution-id=${distributionId} --paths=/*`)
+    execSync(`aws cloudfront create-invalidation --distribution-id=${distributionId} --paths=/*`)
 }
 
 export default deploy
