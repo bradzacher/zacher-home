5fb597f6d0f6d9a42ab7ca6401a36ff6feac29fc
Brad Zacher <brad.zacher@gmail.com>
2025-08-10 11:43:13 +0930

    fix empty wakatime graph

--- scripts/generate/wakatime/generateGraph.ts
+++ scripts/generate/wakatime/generateGraph.ts
@@ -1,16 +1,26 @@
 import fs from 'fs';
 import path from 'node:path';
+import { setTimeout } from 'node:timers/promises';
 
 import { assetsPath } from '../../createBuildFolder';
 
-async function generateGraph(): Promise<void> {
+async function fetchSvg(): Promise<string> {
   const response = await fetch(
     'https://wakatime.com/share/@bradzacher/49374896-4333-4a7d-81f6-97e5557bca12.svg',
     {
       method: 'get',
     },
   );
-  const rawSvg = await response.text();
+  return response.text();
+}
+
+async function generateGraph(): Promise<void> {
+  let rawSvg = await fetchSvg();
+  if (rawSvg.includes('updating in background')) {
+    await setTimeout(5000);
+    rawSvg = await fetchSvg();
+  }
+
   fs.writeFileSync(path.join(assetsPath, 'WakatimeChart.svg'), rawSvg, 'utf8');
 
   console.info('Generated new WakatimeChart.svg');
